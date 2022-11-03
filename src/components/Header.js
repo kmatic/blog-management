4df713af5bbc7454023735bc6140eae767ee2fba
Kristopher <kristopher_matic@hotmail.com>
2022-11-03 12:03:23 -0700

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>Blog-CMS</h1>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/styles/globalStyles.js
@@ -0,0 +1,52 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    word-wrap: break-word;
+    }
+
+    body {
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        background-color: var(--light);
+        text-align: center;
+    }
+    
+    code {
+        font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+        monospace;
+    }
+
+    button {
+        border: none;
+        outline: none;
+        color: inherit;
+        font-family: inherit;
+        font-size: inherit;
+        cursor: pointer;
+    }
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+        line-height: 1.2;
+    }
+    
+    html {
+        line-height: 1.6;
+    }
+`;
+
+export default GlobalStyle;
